2b14fe2998909def4f33ca94ef9bf725d0d193a6
Dev1822 <dev.d.patel.cg@gmail.com>
2026-04-17 14:40:43 +0530

    feat: add create multiple note endpoint (POST /api/notes/bulk)

--- src/controllers/note.controller.js
+++ src/controllers/note.controller.js
@@ -17,3 +17,21 @@ exports.createNote = async (req, res) => {
         });
     }
 };
+
+exports.createBulk=async(req,res)=>{
+    try{
+        const newNotes=await Note.insertMany(req.body["notes"]);
+        const length=req.body["notes"].length;
+        res.status(201).json({
+            success: true,
+            message: `${length} notes created successfully`,
+            data: newNotes
+        });
+    }
+    catch(error){
+        res.status(400).json({
+            success: false,
+            message: error.message
+        });
+    }
+}
--- src/routes/note.routes.js
+++ src/routes/note.routes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const controller = require("../controllers/note.controller");
 
 router.post("/api/notes",controller.createNote);
+router.post("/api/notes/bulk",controller.createBulk);
 
 module.exports = router;
